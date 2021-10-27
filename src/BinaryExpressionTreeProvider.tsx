23fc01110baa0f9306300b65226c45d66e58219a
GeorgeGkas <georgegkas@gmail.com>
2021-10-27 13:15:29 +0300

    feat: create React hook for binary expression tree data structure

---
+++ src/BinaryExpressionTreeProvider.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BinaryExpressionTree } from "./BinaryExpressionTree"
+
+const BinaryExpressionTreeContext = React.createContext<BinaryExpressionTree>(new BinaryExpressionTree())
+
+const BinaryExpressionTreeProvider = (props: React.PropsWithChildren<any>) => (
+  <BinaryExpressionTreeContext.Provider value={new BinaryExpressionTree()}>
+    {props.children}
+  </BinaryExpressionTreeContext.Provider>
+)
+
+const useBinaryExpressionTree = () => React.useContext<BinaryExpressionTree>(BinaryExpressionTreeContext)
+
+export { BinaryExpressionTreeProvider as default, useBinaryExpressionTree }
