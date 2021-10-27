cfae167a0b507f559861f8a5c366b816aec6aa1e
GeorgeGkas <georgegkas@gmail.com>
2021-10-27 12:46:14 +0300

    fix: complex typescript types in react

--- src/renameArgsKeysInPlace.ts
+++ src/renameArgsKeysInPlace.ts
@@ -1,10 +1,8 @@
 import type { Args } from "./ArgsBuilder";
 
-// NOTE: For some reason react typescript is not being able to parse this Typescript
-// syntax. To review it, uncomment the following line.
-// type MappedObjectNamesType<T> = {[argName in keyof T]: string}
+type MappedObjectNamesType<T> = {[argName in keyof T]: string}
 
-export function renameArgsKeysInPlace<T>(obj: Args, newKeys: /*MappedObjectNamesType<Args>*/ {[key: string]: string}): Args {
+export function renameArgsKeysInPlace<T>(obj: Args, newKeys: MappedObjectNamesType<Args>): Args {
   const keyValues = Object.keys(obj).map(key => (
     { [key in newKeys ? newKeys[key] : key]: obj[key] }
   ));
